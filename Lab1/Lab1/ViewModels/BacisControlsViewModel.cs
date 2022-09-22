7ea5d939531ebeac974601119c741cfeae5e7661
Phuc Hoang <hoangmaithienphuc@gmail.com>
2022-09-22 21:03:13 +0700

    update

---
+++ Lab1/Lab1/ViewModels/BacisControlsViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Lab1.Views;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Lab1.ViewModels
+{
+    public class BasicControlsViewModel : BaseViewModel
+    {
+        public Command SubmitCommand { get; }
+        public Command CancelCommand { get; }
+        public BasicControlsViewModel()
+        {
+            Title = "Basic Controls";
+            SubmitCommand = new Command(SubmitClicked);
+            CancelCommand = new Command(CancelClicked);
+        }
+
+        private async void SubmitClicked(object obj)
+        {
+            await App.Current.MainPage.DisplayAlert("Alert", "Submitted", "OK");
+        }
+        private async void CancelClicked(object obj)
+        {
+            await App.Current.MainPage.DisplayAlert("Alert", "Cancelled", "OK");
+        }
+    }
+}
---
+++ Lab1/Lab1/Views/BasicControlsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lab1.Views
+{
+    public partial class BasicControlsPage : ContentPage
+    {
+        public BasicControlsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
