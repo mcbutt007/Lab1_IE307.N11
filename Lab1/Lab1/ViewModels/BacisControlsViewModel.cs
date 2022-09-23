1f9f0c8109185efcb9436dc138d8d5dbeff3756d
Phuc Hoang <hoangmaithienphuc@gmail.com>
2022-09-23 08:03:19 +0700

    update

--- Lab1/Lab1/ViewModels/BacisControlsViewModel.cs
+++ Lab1/Lab1/ViewModels/BacisControlsViewModel.cs
@@ -3,19 +3,47 @@ using Lab1.Views;
 using System.Windows.Input;
 using Xamarin.Essentials;
 using Xamarin.Forms;
+using System.ComponentModel;
 
 namespace Lab1.ViewModels
 {
-    public class BasicControlsViewModel : BaseViewModel
+    public class BasicControlsViewModel : INotifyPropertyChanged
     {
-        public Command SubmitCommand { get; }
-        public Command CancelCommand { get; }
+        private string entUsrName = string.Empty, entUsrEmail = string.Empty, 
+            entUsrPhone = string.Empty, dpkUsrDOB = string.Empty, 
+            swUsrGender = string.Empty;
+        private bool isToggled = false;
+        public String EntUsrName
+        {
+            get { return entUsrName; }
+        }
+        public string EntUsrEmail
+        {
+            get { return entUsrEmail; }
+        }
+        public string EntUsrPhone
+        {
+            get { return entUsrPhone; }
+        }
+        public string DpkUsrDOB
+        {
+            get { return dpkUsrDOB; }
+        }
+
+        public string SwUsrGender
+        {
+            get { return swUsrGender; }
+        }
+        public Command SubmitBtn { get; }
+        public Command CancelBtn { get; }
+        public String Title { get { return "Basic Controls"; } }
         public BasicControlsViewModel()
         {
-            Title = "Basic Controls";
-            SubmitCommand = new Command(SubmitClicked);
-            CancelCommand = new Command(CancelClicked);
+            SubmitBtn = new Command(SubmitClicked);
+            CancelBtn = new Command(CancelClicked);
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
 
         private async void SubmitClicked(object obj)
         {
